fca2fa7d4ef0b90395364353de182dbb9603ed90
NaveOWO <pianowell96@gmail.com>
2022-11-12 00:39:05 +0900

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+### Commit Convention
+
+- [ feat ] 새로 추가된 기능
+- [ refactor ] 기능에 변화는 없지만 더 좋은 코드로 리팩토링 했을 때
+- [ fix ] 버그를 수정했을 때
+- [ design ] css와 스타일 관련된 부분
+- [ etc ] 그외 잡일들~
---
+++ src/style/theme.js
@@ -0,0 +1,26 @@
+import { css, CSSProp } from 'styled-components';
+
+const colors = {
+  // sub_green1: "#7DE0B9",
+};
+
+// function FONT({ weight, size, lineHeight, letterSpacing, notoSans }: Font): string {
+//     return `
+//       font-family: ${notoSans ? "Noto Sans" : "Pretendard"};
+//       font-weight: ${weight};
+//       font-size: ${size}rem;
+//       line-height: ${lineHeight}%;
+//       letter-spacing: -0.0${letterSpacing}rem;
+//     `;
+//   }
+
+const fonts = {
+  //   h1: FONT({ weight: 600, size: 2.6, lineHeight: 140, letterSpacing: 4 }),
+};
+
+const theme = {
+  colors,
+  fonts,
+};
+
+export default theme;
