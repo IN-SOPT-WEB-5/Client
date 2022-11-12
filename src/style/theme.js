ee3d7efd8c7a2a37f58d3c407772f532fea82191
NaveOWO <pianowell96@gmail.com>
2022-11-13 02:34:27 +0900

    init project

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,21 +1,32 @@
 import { css, CSSProp } from 'styled-components';
 
 const colors = {
-  // sub_green1: "#7DE0B9",
+  main_purple: '#503396',
+  main_darken: '#351f66',
+  sub_mint: '#73bcc7',
+  sub_blue: '#344690',
+  gray1: '#222222',
+  gray2: '#444444',
+  gray3: '#666666',
+  gray4: '#888888',
+  gray5: '#aaaaaa',
+  white: '#ffffff',
 };
 
-// function FONT({ weight, size, lineHeight, letterSpacing, notoSans }: Font): string {
-//     return `
-//       font-family: ${notoSans ? "Noto Sans" : "Pretendard"};
-//       font-weight: ${weight};
-//       font-size: ${size}rem;
-//       line-height: ${lineHeight}%;
-//       letter-spacing: -0.0${letterSpacing}rem;
-//     `;
-//   }
+function FONT({ weight, size, lineHeight }) {
+  return `
+      font-weight: ${weight};
+      font-size: ${size}rem;
+      line-height: ${lineHeight}%;
+    `;
+}
 
 const fonts = {
-  //   h1: FONT({ weight: 600, size: 2.6, lineHeight: 140, letterSpacing: 4 }),
+  headline1: FONT({ weight: 400, size: 3, lineHeight: 110 }),
+  headline2: FONT({ weight: 400, size: 2.8, lineHeight: 111 }),
+  headline3: FONT({ weight: 400, size: 2, lineHeight: 110 }),
+  body1_bold: FONT({ weight: 600, size: 1.5, lineHeight: 153 }),
+  body1: FONT({ weight: 400, size: 1.3, lineHeight: 154 }),
 };
 
 const theme = {
