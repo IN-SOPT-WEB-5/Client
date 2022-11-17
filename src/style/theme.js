8f454e07e8b3c0bd96c383a8ffb8556f2ff5a0bd
NaveOWO <pianowell96@gmail.com>
2022-11-17 11:07:44 +0900

    [ design ] theme 추가

--- src/style/theme.js
+++ src/style/theme.js
@@ -28,6 +28,7 @@ const fonts = {
   headline3: FONT({ weight: 400, size: 2, lineHeight: 110 }),
   body1_bold: FONT({ weight: 600, size: 1.5, lineHeight: 153 }),
   body1: FONT({ weight: 400, size: 1.3, lineHeight: 154 }),
+  body2: FONT({ weight: 400, size: 1.3, lineHeight: 154 }),
 };
 
 const theme = {
