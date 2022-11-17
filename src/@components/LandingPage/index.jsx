346bb0908d418ce428a5da2e47a5796eec912059
NaveOWO <pianowell96@gmail.com>
2022-11-17 20:15:26 +0900

    [ feat ] index 파일 생성

---
+++ src/@components/LandingPage/index.jsx
@@ -0,0 +1,15 @@
+import BoxOffice from './BoxOffice';
+import Benefits from './Benefits';
+import Curation from './Curation';
+import Guide from './Guide';
+
+export default function Landingpage() {
+  return (
+    <>
+      <BoxOffice />
+      <Benefits />
+      <Curation />
+      <Guide />
+    </>
+  );
+}
