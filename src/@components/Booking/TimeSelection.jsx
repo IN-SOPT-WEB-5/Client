bbf201d0e144ae7773ea2dd72e5f40547f5a18df
Hennization <shy6295@ewhain.net>
2022-11-18 16:18:48 +0900

    [ feat ] 시간 UI 구현

--- src/@components/Booking/TimeSelection.jsx
+++ src/@components/Booking/TimeSelection.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import SpecialType from './SpecialType';
+import CalendarSvg from '../../assets/Calendar.svg';
 
 export default function TimeSelection() {
   return (
@@ -8,6 +9,13 @@ export default function TimeSelection() {
         <h3>시간</h3>
       </St.SectionTitle>
       <SpecialType />
+      <St.TimeSelectArea>
+        <St.CalendarIcon src={CalendarSvg} alt="달력 아이콘" />
+        <St.TimeSelectExplain>
+          날짜, 영화, 극장을 선택하시면 <br />
+          상영시간표를 비교하여 볼 수 있습니다.
+        </St.TimeSelectExplain>
+      </St.TimeSelectArea>
     </St.Root>
   );
 }
@@ -15,6 +23,7 @@ export default function TimeSelection() {
 const St = {
   Root: styled.section`
     width: 100%;
+    height: 100%;
   `,
   SectionTitle: styled.div`
     ${({ theme }) => theme.fonts.headline3};
@@ -27,5 +36,20 @@ const St = {
     align-items: center;
     justify-content: center;
     border-left: 1px solid ${({ theme }) => theme.colors.main_darken};
+  `,
+
+  TimeSelectArea: styled.article`
+    width: 100%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  `,
+  CalendarIcon: styled.img``,
+  TimeSelectExplain: styled.p`
+    ${({ theme }) => theme.fonts.body1};
+    color: ${({ theme }) => theme.colors.gray3};
+    text-align: center;
   `,
 };
