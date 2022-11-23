429abd358fb3b6e3d65cbf386313ac95114b537d
NaveOWO <pianowell96@gmail.com>
2022-11-24 02:17:12 +0900

    [ fix ] router 파일 경로 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import BoxOffice from '../src/@components/boxoffice/Index';
-
+import BookingPage from './@components/BookingPage';
+import Landingpage from './@components/LandingPage';
+import SeatSelectingPage from './@components/SeatSelectingPage/SeatSelectingPage';
 
 export default function Router() {
   return (
@@ -14,7 +16,6 @@ export default function Router() {
         <Route path="/booking" element={<BookingPage />} />
         <Route path="/" element={<Landingpage />} />
         <Route path="/select" element={<SeatSelectingPage />} />
-
       </Routes>
     </BrowserRouter>
   );
