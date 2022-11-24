b864621a5ef1265a2780a1badca49aa74e4dd28a
Hennization <shy6295@ewhain.net>
2022-11-24 14:31:16 +0900

    [ feat ] 예약정보 select 페이지로 넘겨주기

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,20 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import BoxOffice from '../src/@components/boxoffice/Index';
-
+import BookingPage from '../src/@components/BookingPage/index';
+import Landingpage from '../src/@components/LandingPage/index';
+import SeatSelectingPage from '../src/@components/SeatSelectingPage/SeatSelectingPage';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/boxoffice" element={<BoxOffice />} />
-
-        <Route path="/boxoffice" element={<BoxOffice />} />
-
         <Route path="/booking" element={<BookingPage />} />
         <Route path="/" element={<Landingpage />} />
         <Route path="/select" element={<SeatSelectingPage />} />
-
       </Routes>
     </BrowserRouter>
   );
