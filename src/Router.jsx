50f9eb2ac32026766c30e5ad06badcad7c9bf774
NaveOWO <pianowell96@gmail.com>
2022-11-25 11:46:31 +0900

    [ fix ] esLint 오류 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,19 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import BoxOffice from '../src/@components/boxoffice/Index';
-
-
 import BookingPage from '../src/@components/BookingPage/index';
 import Landingpage from '../src/@components/LandingPage/index';
 import SeatSelectingPage from '../src/@components/SeatSelectingPage/SeatSelectingPage';
-
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-
-
         <Route path="/booking" element={<BookingPage />} />
         <Route path="/" element={<Landingpage />} />
         <Route path="/select" element={<SeatSelectingPage />} />
