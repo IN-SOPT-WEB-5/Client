1a93963cdd52adba5f8f97be725aceadcd5949a3
borimong <kimhyunsu11@naver.com>
2022-11-26 00:44:12 +0900

    fix: 라우터..

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,16 +3,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BoxOffice from './@components/BoxofficePage/index';
 import BookingPage from '../src/@components/BookingPage/index';
 import Landingpage from '../src/@components/LandingPage/index';
-import SeatSelectingPage from './@components/SeatSelectingPage/index';
+import SeatSelectingPage from './@components/SeatSelectingPage/index.jsx';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landingpage />} />
-        <Route path="/boxoffice" element={<BoxOffice />} />
-        <Route path="/booking" element={<BookingPage />} />
-        <Route path="/select" element={<SeatSelectingPage />} />
+        <Route exact path="/" element={<Landingpage />} />
+        <Route exact path="/boxoffice" element={<BoxOffice />} />
+        <Route exact path="/booking" element={<BookingPage />} />
+        <Route exact path="/select" element={<SeatSelectingPage />} />
       </Routes>
     </BrowserRouter>
   );
